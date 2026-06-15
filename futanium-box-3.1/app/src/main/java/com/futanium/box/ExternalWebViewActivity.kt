9ec0d896e6e34afbeecdec226e6b31186076ce7c
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-06-15 01:27:54 -0300

    Atualizar o ExternalWebViewActivity.kt

--- futanium-box-3.1/app/src/main/java/com/futanium/box/ExternalWebViewActivity.kt
+++ futanium-box-3.1/app/src/main/java/com/futanium/box/ExternalWebViewActivity.kt
@@ -11,7 +11,7 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.futanium.box.databinding.ActivityExternalWebviewBinding
+import com.futaniumbox.players.databinding.ActivityExternalWebviewBinding
 
 class ExternalWebViewActivity : AppCompatActivity() {
 
