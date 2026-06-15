e25d6184e8efa33340e0a21a7c0d2a0078fb8f5f
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-06-14 23:15:52 -0300

    Criar Channel.kt

---
+++ futanium-box-3.1/app/src/main/java/com/futanium/box/model/Channel.kt
@@ -0,0 +1,16 @@
+package com.futanium.box.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Channel(
+
+    @SerializedName("name")
+    val name: String? = null,
+
+    @SerializedName("link")
+    val link: String? = null,
+
+    @SerializedName("captureM3u8")
+    val captureM3u8: Boolean = false
+
+)
